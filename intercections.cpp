cfca69ff5b89abec5a17776c881cb86e05d67aa3
Rennan Gaio <rennan.gaio12@gmail.com>
2018-06-28 19:50:06 -0300

    correcting bugs

--- intercections.cpp
+++ intercections.cpp
@@ -19,20 +19,8 @@ GLfloat lerp(GLfloat p1, GLfloat p2,GLfloat t){
 point* calcQLintersects(point p1, point p2, point p3, point a1, point a2) {
   point intersections[2];
 
-	//point p1, p2, p3, a1, a2;
 	point normal, c2, c1, c0;
 
-	// changing struct of points to be more human readable
-	// p1.x=px1;
-	// p1.y=py1;
-	// p2.x=px2;
-	// p2.y=py2;
-	// p3.x=px3;
-	// p3.y=py3;
-	// a1.x=ax1;
-	// a1.y=ay1;
-	// a2.x=ax2;
-	// a2.y=ay2;
 
   // inverse line normal
   normal.x= a1.y-a2.y;
@@ -58,40 +46,41 @@ point* calcQLintersects(point p1, point p2, point p3, point a1, point a2) {
   GLfloat c=(normal.x*c0.x + normal.y*c0.y + coefficient)/a;
 
   // solve the roots
-  GLfloat roots[2]= {};
+  GLfloat roots[2]= {NULL, NULL};
   GLfloat d = b*b-4*c;
   if(d>0){
     roots[0] = (-b+sqrt(d))/2;
     roots[1] = (-b-sqrt(d))/2;
   }else if(d==0){
     roots[0] = -b/2;
-		roots[1] = -b/2;
   }
 
   // calc the solution points
   for(int i=0;i<2;i++){
-    GLfloat minX = fmin(a1.x,a2.x);
-    GLfloat minY = fmin(a1.y,a2.y);
-    GLfloat maxX = fmax(a1.x,a2.x);
-    GLfloat maxY = fmax(a1.y,a2.y);
-    GLfloat t = roots[i];
-    if (t>=0 && t<=1) {
-      // possible point -- pending bounds check
+    if (roots[i]!=NULL){
+      GLfloat minX = fmin(a1.x,a2.x);
+      GLfloat minY = fmin(a1.y,a2.y);
+      GLfloat maxX = fmax(a1.x,a2.x);
+      GLfloat maxY = fmax(a1.y,a2.y);
+      GLfloat t = roots[i];
+      if (t>=0 && t<=1) {
+        // possible point
 
-      point i_point;
-      i_point.x = lerp(lerp(p1.x,p2.x,t),lerp(p2.x,p3.x,t),t);
-      i_point.y = lerp(lerp(p1.y,p2.y,t),lerp(p2.y,p3.y,t),t);
+        point i_point;
+        i_point.x = lerp(lerp(p1.x,p2.x,t),lerp(p2.x,p3.x,t),t);
+        i_point.y = lerp(lerp(p1.y,p2.y,t),lerp(p2.y,p3.y,t),t);
 
-      // bounds checks
-      if(a1.x==a2.x && i_point.y>=minY && i_point.y<=maxY){
-        // vertical line
-        intersections[i]=i_point;
-      }else if(a1.y==a2.y && i_point.x>=minX && i_point.x<=maxX){
-        // horizontal line
-        intersections[i]=i_point;
-      }else if(i_point.x>=minX && i_point.y>=minY && i_point.x<=maxX && i_point.y<=maxY){
-        // line passed bounds check
-        intersections[i]=i_point;
+        // bounds checks
+        if(a1.x==a2.x && i_point.y>=minY && i_point.y<=maxY){
+          // vertical line
+          intersections[i]=i_point;
+        }else if(a1.y==a2.y && i_point.x>=minX && i_point.x<=maxX){
+          // horizontal line
+          intersections[i]=i_point;
+        }else if(i_point.x>=minX && i_point.y>=minY && i_point.x<=maxX && i_point.y<=maxY){
+          // line passed bounds check
+          intersections[i]=i_point;
+        }
       }
     }
   }
